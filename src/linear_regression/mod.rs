9a3a90acb1a477c1c6c9772912f8c4e333407bef
Chen Liang <chen.liang.cn@outlook.com>
2024-03-30 16:35:39 +0000

    use the quickest implementaion

--- src/linear_regression/mod.rs
+++ src/linear_regression/mod.rs
@@ -1,7 +1,7 @@
 //! Implementation of linear regression
 use crate::read_data;
 use std::path::Path;
-use std::time::Instant;
+// use std::time::Instant;
 
 pub mod cost_functions;
 pub mod gradient_descent;
@@ -25,63 +25,55 @@ pub fn sample_run(input_file_path: &Path) {
         x[0].len() as f32 / 10.0
     };
 
-    let start = Instant::now();
-    match gradient_descent::get_thetas(&x, &y, alpha, &mut theta, ITERATIONS) {
-        Ok(theta) => {
-            println!("Found thetas using Gradient Descent with learning speed {} and {} number of iterations: {:?}", alpha, ITERATIONS, &theta[1..]);
-        }
-        Err(e) => panic!("{}", e.get_ref().unwrap()),
-    }
-    let duration = start.elapsed();
-    println!("Time elapsed in get_thetas() is: {:?}\n", duration);
+    // match gradient_descent::get_thetas(&x, &y, alpha, &mut theta, ITERATIONS) {
+    //     Ok(theta) => {
+    //         println!("Found thetas using Gradient Descent with learning speed {} and {} number of iterations: {:?}", alpha, ITERATIONS, &theta[1..]);
+    //     }
+    //     Err(e) => panic!("{}", e.get_ref().unwrap()),
+    // }
 
-    let start = Instant::now();
-    match gradient_descent::get_thetas_hypothesis_function(&x, &y, alpha, &mut theta, ITERATIONS) {
-        Ok(theta) => {
-            println!("Hypothesis function");
-            println!("Found thetas using Gradient Descent with learning speed {} and {} number of iterations: {:?}", alpha, ITERATIONS, &theta[1..]);
-        }
-        Err(e) => panic!("{}", e.get_ref().unwrap()),
-    }
-    let duration = start.elapsed();
-    println!("Time elapsed in get_thetas_hypothesis_function() is: {:?}\n", duration);
+    // match gradient_descent::get_thetas_hypothesis_function(&x, &y, alpha, &mut theta, ITERATIONS) {
+    //     Ok(theta) => {
+    //         println!("Hypothesis function");
+    //         println!("Found thetas using Gradient Descent with learning speed {} and {} number of iterations: {:?}", alpha, ITERATIONS, &theta[1..]);
+    //     }
+    //     Err(e) => panic!("{}", e.get_ref().unwrap()),
+    // }
 
+
+    // Flattened X
     // Read data from file
-    let (flattened_x, num_feat, y) = match read_data::get_data_flat_x(input_file_path) {
-        Ok((flattened_x, num_feat, y)) => (flattened_x, num_feat, y),
-        Err(e) => panic!("{}", e.get_ref().unwrap()),
-    };
-
-    // set the learning rate = no of features / 10
-    let alpha = if num_feat < 3 {
-        0.01
-    } else {
-        num_feat as f32 / 10.0
-    };
-
-    let start = Instant::now();
-    match gradient_descent::get_thetas_flatten_x(
-        &flattened_x,
-        &y,
-        alpha,
-        num_feat,
-        &mut theta,
-        ITERATIONS,
-    ) {
-        Ok(theta) => {
-            println!("Flattened X");
-            println!(
-                "Gradient Descent with learning speed {} and {} number of iterations:\n {:?}",
-                alpha,
-                ITERATIONS,
-                &theta[1..]
-            );
-        }
-        Err(e) => panic!("{}", e.get_ref().unwrap()),
-    }
-
-    let duration = start.elapsed();
-    println!("Time elapsed in get_thetas_flatten_x() is: {:?}", duration);
+    // let (flattened_x, num_feat, y) = match read_data::get_data_flat_x(input_file_path) {
+    //     Ok((flattened_x, num_feat, y)) => (flattened_x, num_feat, y),
+    //     Err(e) => panic!("{}", e.get_ref().unwrap()),
+    // };
+    //
+    // // set the learning rate = no of features / 10
+    // let alpha = if num_feat < 3 {
+    //     0.01
+    // } else {
+    //     num_feat as f32 / 10.0
+    // };
+    //
+    // let mut theta = vec![0.0; num_feat]; // set theta 0 and theta 1 to 0.0
+    // match gradient_descent::get_thetas_flatten_x(
+    //     &flattened_x,
+    //     &y,
+    //     alpha,
+    //     num_feat,
+    //     &mut theta,
+    //     ITERATIONS,
+    // ) {
+    //     Ok(theta) => {
+    //         println!(
+    //             "Gradient Descent with learning speed {} and {} number of iterations:\n {:?}",
+    //             alpha,
+    //             ITERATIONS,
+    //             &theta[1..]
+    //         );
+    //     }
+    //     Err(e) => panic!("{}", e.get_ref().unwrap()),
+    // }
 
     // match normal_equation::get_theta(&x, &y) {
     //     Ok(theta) => {
